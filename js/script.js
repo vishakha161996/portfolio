774f5b682c33db3baaae9c4fc977d03065235a4d
vishakha161996 <139291780+vishakha161996@users.noreply.github.com>
2024-02-12 03:27:39 +0530

    update

--- js/script.js
+++ js/script.js
@@ -12,20 +12,3 @@ function smoothScroll(event) {
     behavior: "smooth"
   });
 }
-
-
-
-// ------------------------ Scroll to top ------------------------
-// Get the scroll-to-top button element
-const scrollToTopBtn = document.getElementById('scrollToTopBtn');
- 
-// Add a click event listener to the button
-scrollToTopBtn.addEventListener('click', scrollToTop);
- 
-// Function to scroll to top smoothly
-function scrollToTop() {
-  window.scrollTo({
-    top: 0,
-    behavior: 'smooth'
-  });
-}
